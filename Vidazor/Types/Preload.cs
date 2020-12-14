339baab1b7960b55688d924beffd1f2792540650
Arad Aral <AmirHosseinHmd@outlook.com>
2020-12-14 14:36:19 +0330

    Initial commit.

---
+++ Vidazor/Types/Preload.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidazor.Types
+{
+    public enum Preload
+    {
+        /// <summary>
+        /// Indicates that the whole video can be loaded.
+        /// </summary>
+        Auto,
+
+        /// <summary>
+        /// Indicates that only the metadata (e.g. length) of the video is loaded.
+        /// </summary>
+        Metadata,
+
+        /// <summary>
+        /// Indicates that the video should not be preloaded.
+        /// </summary>
+        None,
+    }
+}
